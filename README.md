b64adba0be6e4f24867c1cef6c50083b28c6c730
guodongxiaren <879231132@qq.com>
2015-02-10 17:55:55 +0800

    折腾了一天autotools 今天没怎么学网络编程，信号也没学。一直在和autotools斗法

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # UNP
+
+##编译流程
+- autoscan：生成`configure.scan`
+- mv configure.scan configure.ac
+- 编辑configure.ac（加入**AM_INIT_AUTOMAKE**）
+- aclocal：利用`configure.ac`生成一些宏
+- autoheader:生成`config.h.in`
+- autoconf:生成可执行文件`configure`
+- 新建并编辑`Makefile.am`
+- automake --add-missing：生成`makefile.in`
+- ./configure:生成头文件`config.h`以及`makefile`
---
+++ lib/unp.h
@@ -0,0 +1,23 @@
+#ifndef _UNP_H
+#define _UNP_H
+
+#include "../config.h"
+#include <time.h>
+#include <stdio.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <sys/un.h>
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/uio.h>
+#include <sys/time.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#endif
